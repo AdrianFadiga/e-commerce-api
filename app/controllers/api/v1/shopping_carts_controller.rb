0f3a02ec42487d390adf2ac20136f9e21fd12a79
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-03-23 00:08:56 -0300

    feat: simplificação na lógica

--- app/controllers/api/v1/shopping_carts_controller.rb
+++ app/controllers/api/v1/shopping_carts_controller.rb
@@ -1,29 +1,36 @@
 module Api::V1
   class ShoppingCartsController < ApplicationController
     before_action :authenticate_api_user!
-    before_action :set_shopping_cart
+    before_action :set_shopping_cart_products
 
     def show
-      render json: @shopping_cart.shopping_cart_products.as_json(include: :product)
+      render json: @shopping_cart_products.as_json(include: :product)
     end
 
     def update
-      shopping_cart_product = @shopping_cart.shopping_cart_products.find_or_initialize_by(product_id: params[:product_id])
-      shopping_cart_product.assign_attributes(quantity: params[:quantity])
-      if shopping_cart_product.save
-        render json: @shopping_cart.shopping_cart_products
+      new_product = @shopping_cart_products.find_or_initialize_by(product_id: params[:product_id])
+      new_product.assign_attributes(quantity: params[:quantity])
+      if new_product.save
+        render json: @shopping_cart_products
       else
-        render json: { error: shopping_cart_product.errors }, status: :unprocessable_entity
+        render json: { error: new_product.errors }, status: :unprocessable_entity
       end
     end
 
-    def destroy; end
+    def destroy
+      product = @shopping_cart_products.find_by(product_id: params[:product_id])
+      if product.nil?
+        render json: { error: 'Product not found' }, status: :unprocessable_entity
+      else
+        product.destroy
+        render status: :ok
+      end
+    end
 
     private
 
-    def set_shopping_cart
-      @shopping_cart = current_api_user.shopping_cart
-      render json: { shopping_cart: 'Shopping Cart not found' }, status: :not_found if @shopping_cart.nil?
+    def set_shopping_cart_products
+      @shopping_cart_products = current_api_user.shopping_cart.shopping_cart_products
     end
 
     def shopping_cart_params
